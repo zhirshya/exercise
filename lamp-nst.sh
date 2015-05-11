5534ed8d4a525c2a4ef2232a504341446f94ecf0
r <r@ergnmbhvgrfn.bit>
2015-05-11 16:57:21 +0300

    Install LAMP in Ubuntu Server 14.04 LTS

--- lamp-nst.sh
+++ lamp-nst.sh
@@ -1,7 +1,7 @@
-#fedora
 #OpenCart
 https://www.rosehosting.com/blog/install-opencart-2-on-an-ubuntu-14-04-vps-with-nginx-and-php-fpm/
 
+#fedora
 sudo dnf install httpd -y
 
 systemctl enable httpd
@@ -24,6 +24,12 @@ Restart firewalld service:
 
 firewall-cmd --reload
 
+#ubuntu
+sudo apt-get install apache2 apache2-utils
+
+sudo service apache2 restart
+
+#mariadb
 sudo dnf install mariadb mariadb-server -y
 
 Enable MariaDB at startup:
@@ -34,10 +40,45 @@ Start the daemon:
 Set MariaDB root password:
 	mysql_secure_installation
 
+#mariadb ubuntu
+#sudo systemctl stop mysql  #fedora only?
+sudo service mysql stop
+sudo apt-get remove --purge mysql-server mysql-client mysql-common
+sudo apt-get autoremove
+sudo apt-get autoclean
+sudo rm -rf /var/lib/mysql/
+sudo rm -rf /etc/mysql/
+
+sudo apt-get install mariadb-server
+
+mysql_secure_installation
+
 #PHP
 sudo dnf install php phpmyadmin -y
 
 #PHP module #php5-mysqlnd <==> php5-mysqld
 sudo apt-get install php5-fpm php5-cli php5-mcrypt php5-gd php5-mysqlnd php5-curl
 sudo php5enmod mcrypt
+
+#PHP ubuntu
+sudo apt-get install libapache2-mod-php5 php5 php5-mysql php-pear php5-gd php5-mcrypt php5-curl
+
+/*
+dbconfig-common: writing config to /etc/dbconfig-common/phpmyadmin.conf
+
+Creating config file /etc/dbconfig-common/phpmyadmin.conf with new version
+
+Creating config file /etc/phpmyadmin/config-db.php with new version
+granting access to database phpmyadmin for phpmyadmin@localhost: success.
+verifying access for phpmyadmin@localhost: success.
+creating database phpmyadmin: success.
+verifying database phpmyadmin exists: success.
+populating database via sql...  done.
+dbconfig-common: flushing administrative password
+apache2_invoke: Enable configuration phpmyadmin
+ * Reloading web server apache2                                                                                                                   * 
+ * Reloading web server apache2                                                                                                                   * 
+*/
+
+sudo apt-get install phpmyadmin 
 
