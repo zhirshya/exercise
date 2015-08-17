eae0e85f6b5a70a2be6bc45254d6b9bd9c7de1e7
rrrgh <r@ergnmbhvgrfn.localdomain>
2015-08-17 16:52:25 +0300

    + std::chrono

--- sum_indvdl_figr.cpp
+++ sum_indvdl_figr.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <cmath>
+#include <chrono>
+
+using namespace std;
 
 int dosum(int param)
 {
@@ -15,7 +18,30 @@ int dosum(int param)
 
 int main()
 {
+	cout << "system_clock" << endl;
+	cout << chrono::system_clock::period::num << endl;
+	cout << chrono::system_clock::period::den << endl;
+	cout << "steady = " << boolalpha << chrono::system_clock::is_steady << endl << endl;
+ 
+	cout << "high_resolution_clock" << endl;
+	cout << chrono::high_resolution_clock::period::num << endl;
+	cout << chrono::high_resolution_clock::period::den << endl;
+	cout << "steady = " << boolalpha << chrono::high_resolution_clock::is_steady << endl << endl;
+ 
+	cout << "steady_clock" << endl;
+	cout << chrono::steady_clock::period::num << endl;
+	cout << chrono::steady_clock::period::den << endl;
+	cout << "steady = " << boolalpha << chrono::steady_clock::is_steady << endl << endl;
+
+	auto start = chrono::steady_clock::now();
 	//unsigned tmp = dosum(-237961);
 	std::cout << "sum of all individual figures of " << -237961 << " is:[" << dosum(-237961) << "]" << std::endl;
+
+	auto end = chrono::steady_clock::now();
+	auto diff = end - start;
+	cout << chrono::duration <double, nano> (diff).count() << " ns" << endl;
+	cout << chrono::duration <double, micro> (diff).count() << " us" << endl;
+	cout << chrono::duration <double, milli> (diff).count() << " ms" << endl;
+
 	return 0;
 }
