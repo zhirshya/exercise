9ace7ab494b5caaea90387de9c314da63eb5c93a
rrrgh <r@ergnmbhvgrfn.localdomain>
2015-08-27 14:57:44 +0300

    + debug cout

--- fnd++.cpp
+++ fnd++.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <stdlib.h>
+#include <stdlib.h> //system()
 
 //https://www.youtube.com/watch?v=Q_0_1mKTlnY
 //uuid may contain -, so check the length of resulting substring!
@@ -8,21 +8,22 @@
 //echo https://www.youtube.com/watch?v=Q_0_1mKTlnY | gawk -F '=' '{print $2}'
 
 //1. c++ 11 c++ 14 array to vector
-//2. c++ 11 c++ 14 string concatenation
-//3. c++ 11 c++ 14 string to char*
 int main(int argc, char* argv[])
 {
+    if(2 <= argc)
+    {
 	using namespace std;
-	cout << argc << endl;
+	cout << "argc: " << argc << endl;
 	string arg{argv[1]};
-	cout << arg << endl;
+	cout << "1st arg: [" << arg << "]" << endl;
 
 	arg = arg.substr(arg.find('=')+1);
-	cout << arg << endl;
+	cout << "substr to send to system(): [" << arg << "]" << endl;
 	string syscmd{"find /home/r/tekvid -iname '*"};
 	syscmd.append(arg).append("*'");
-	cout << syscmd << endl;
+	cout << "invoke shell command: [" << syscmd << "]" << endl;
 
 	system(syscmd.c_str());
-	return 0;
+    }
+    return 0;
 }
