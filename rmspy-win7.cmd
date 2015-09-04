863de33ebf2b772154c7c142b39e55430ecd8d7d
rrrgh <r@ergnmbhvgrfn.localdomain>
2015-09-04 11:59:44 +0300

    rm spy updates

---
+++ rmspy-win7.cmd
@@ -0,0 +1,5 @@
+wusa /uninstall /kb:3068708 /quiet /norestart
+wusa /uninstall /kb:3022345 /quiet /norestart
+wusa /uninstall /kb:3075249 /quiet /norestart
+wusa /uninstall /kb:3080149 /quiet /norestart
+
