0c9fe20dd84fc1e86c37212c4bdfa48141680561
rrrgh <r@ergnmbhvgrfn.localdomain>
2015-09-24 22:16:58 +0900

    убирать

--- strt.sh
+++ strt.sh
@@ -1,17 +1,17 @@
+#1
 gnome-terminal --maximize --tab-with-profile=Unnamed --working-directory=~ --tab-with-profile=Unnamed  --working-directory=/home/r/gthb/неkod --tab-with-profile=Unnamed --working-directory=/home/r/gthb/xrcs --tab-with-profile=Unnamed --working-directory=/home/r/gthb/wbst  > /dev/null 2>&1 &
 
-#google-chrome-stable --ssl-version-min=tls1.2 --incognito http://www.weblio.jp http://www.opencart.com https://httpd.apache.org/docs/2.4/misc/security_tips.html http://gihyo.jp \
-#http://gihyo.jp/admin/serial/01/ubuntu-recipe/0372 \
+#2
+#vlc > /dev/null 2>&1 &
+
+#3
 #https://serverfault.com/questions/357108/what-permissions-should-my-website-files-folders-have-on-a-linux-webserver \
 #https://serverfault.com/questions/224560/best-permissions-ownership-for-apache-document-root \
 #http://www.tecmint.com/secure-files-using-acls-in-linux \
-#https://httpd.apache.org/docs/2.4/urlmapping.html \
-#https://aws.amazon.com/start-ups/ \
 #http://startupguide.com/ \
 #https://www.udemy.com/ecommerce-website-in-php-mysqli \
 #https://www.google.co.jp/search?q=php+html5+ecommerce+website+and+CMS \
 #https://www.youtube.com/results?search_query=php+html5+ecommerce+website+and+CMS \
-#http://getbootstrap.com/getting-started/ \
 #https://jquery.com
 #https://www.youtube.com/watch?v=K7kyhKGANcc&t=10m
 #https://www.youtube.com/watch?v=e6VYRVRoC40&t=5m15s
@@ -21,13 +21,13 @@ gnome-terminal --maximize --tab-with-profile=Unnamed --working-directory=~ --tab
 #herbsutter.com
 #https://github.com/kjellkod/g3log
 #https://progit.org
+
 #https://calomel.org
 #http://ferd.ca/queues-don-t-fix-overload.html
-#http://speakingjs.com/es5/index.html 
+
 #https://github.com/sergei1152/Personal-Website
 #https://github.com/e107inc/e107
 #
-#http://www.realmenrealstyle.com/how-to-tie-necktie-chart/
 #http://www.atailoredsuit.com/match-tie-knot-shirt-collar-style.html
 #https://www.youtube.com/watch?v=Ew0ppa9knyM
 #
@@ -51,9 +51,9 @@ gnome-terminal --maximize --tab-with-profile=Unnamed --working-directory=~ --tab
 #http://eloquentjavascript.net/
 #http://speakingjs.com/es5/index.html
 
-#google-chrome-stable --ssl-version-min=tls1 --incognito http://www.weblio.jp/content/%E6%B1%BA%E6%B8%88 http://www.tecmint.com/create-mobile-friendly-responsive-web-application-using-jquery http://jstherightway.org/ http://www.oreilly.com/catalog/errata.csp?isbn=9781491918661 https://github.com/rwaldron/idiomatic.js http://www.codeproject.com http://rosettacode.org/wiki/Category:Programming_Tasks https://kjellkod.wordpress.com/2012/02/06/exploring-c11-part-1-time/ http://www.codergears.com/Blog/?p=431 http://nerds-central.blogspot.de/2015/08/functional-c-approaching-fibonacci-from.html http://www.codeproject.com/Tips/29524/Generators-in-C http://www.codeproject.com/Articles/669611/C-or-Cplusplus-Console-Program-Code-Generator http://www.enkisoftware.com/devlogpost-20150822-1-Implementing_a_lightweight_task_scheduler.html https://jrruethe.github.io/blog/2015/08/17/yaml-de-slash-serialization-with-boost-fusion/ https://msdn.microsoft.com/en-us/magazine/dn913181.aspx http://www.codeproject.com/Articles/570638/Ten-Cplusplus-Features-Every-Cplusplus-Developer http://www.drdobbs.com/cpp/the-c14-standard-what-you-need-to-know/240169034 http://www.drdobbs.com/cpp/c11s-async-template/240001196 http://sanderrossel.com/mean-web-development-1-mean-the-what-and-why http://www.codeproject.com/Tips/1023227/A-General-Cplusplus-Reflection-Engine-Based-on-C http://glebbahmutov.com/blog/reading-books-using-epics/ www.toeic.or.jp/index.html
+#google-chrome-stable --ssl-version-min=tls1 --incognito http://www.weblio.jp http://www.tecmint.com/create-mobile-friendly-responsive-web-application-using-jquery http://jstherightway.org/ http://www.oreilly.com/catalog/errata.csp?isbn=9781491918661 https://github.com/rwaldron/idiomatic.js http://www.codeproject.com http://rosettacode.org/wiki/Category:Programming_Tasks https://kjellkod.wordpress.com/2012/02/06/exploring-c11-part-1-time/ http://www.codergears.com/Blog/?p=431 http://nerds-central.blogspot.de/2015/08/functional-c-approaching-fibonacci-from.html http://www.codeproject.com/Tips/29524/Generators-in-C http://www.codeproject.com/Articles/669611/C-or-Cplusplus-Console-Program-Code-Generator http://www.enkisoftware.com/devlogpost-20150822-1-Implementing_a_lightweight_task_scheduler.html https://jrruethe.github.io/blog/2015/08/17/yaml-de-slash-serialization-with-boost-fusion/ https://msdn.microsoft.com/en-us/magazine/dn913181.aspx http://www.codeproject.com/Articles/570638/Ten-Cplusplus-Features-Every-Cplusplus-Developer http://www.drdobbs.com/cpp/the-c14-standard-what-you-need-to-know/240169034 http://www.drdobbs.com/cpp/c11s-async-template/240001196 http://sanderrossel.com/mean-web-development-1-mean-the-what-and-why http://www.codeproject.com/Tips/1023227/A-General-Cplusplus-Reflection-Engine-Based-on-C http://glebbahmutov.com/blog/reading-books-using-epics/ www.toeic.or.jp/index.html
 
-google-chrome-stable --ssl-version-min=tls1 --incognito http://www.weblio.jp/content/%E6%B1%BA%E6%B8%88 http://sanderrossel.com/mean-web-development-1-mean-the-what-and-why http://eloquentjavascript.net https://facebook.github.io/react/index.html https://leanpub.com/javascriptallongesix/read http://glebbahmutov.com/blog/perfect-code-zero-effort http://glebbahmutov.com/blog/review-principles-of-oo-javascript http://glebbahmutov.com/blog/categories/process http://glebbahmutov.com/blog/categories/products http://glebbahmutov.com/blog/javascript-closures http://glebbahmutov.com/blog/starting-to-react http://www.ibm.com/developerworks/library/wa-mean1/index.html https://thinkster.io/mean-stack-tutorial http://hapijs.com  > /dev/null 2>&1 &
+google-chrome-stable --ssl-version-min=tls1 --incognito http://www.weblio.jp http://sanderrossel.com/mean-web-development-1-mean-the-what-and-why http://eloquentjavascript.net https://facebook.github.io/react/index.html https://leanpub.com/javascriptallongesix/read http://glebbahmutov.com/blog/perfect-code-zero-effort http://glebbahmutov.com/blog/review-principles-of-oo-javascript http://glebbahmutov.com/blog/categories/process http://glebbahmutov.com/blog/categories/products http://glebbahmutov.com/blog/starting-to-react http://www.ibm.com/developerworks/library/wa-mean1/index.html https://thinkster.io/mean-stack-tutorial http://hapijs.com > /dev/null 2>&1 &
 
 #https://github.com/jlevy/the-art-of-command-line https://unity3d.com http://www.sputnik.ru http://speakingjs.com/es5/index.html 
 #https://hackhands.com/how-to-get-started-on-the-mean-stack/
@@ -64,7 +64,6 @@ google-chrome-stable --ssl-version-min=tls1 --incognito http://www.weblio.jp/con
 #http://vimdoc.sourceforge.net/htmldoc/visual.html
 
 #http://glebbahmutov.com #JavaScript ninja, image processing expert, software quality fanatic
-#http://www.moj.go.jp/ONLINE/IMMIGRATION/16-2.html
 
 #http://blog.jetbrains.com/clion/2015/07/webinar-recording-a-tour-of-modern-c/
 #https://stackoverflow.com/questions/12883493/timing-the-execution-of-statements-c
@@ -79,4 +78,3 @@ google-chrome-stable --ssl-version-min=tls1 --incognito http://www.weblio.jp/con
 #https://isocpp.org/blog/2014/06/stroustrup-lists
 #http://nerds-central.blogspot.de/2015/08/functional-c-approaching-fibonacci-from.html
 
-vlc > /dev/null 2>&1 &
