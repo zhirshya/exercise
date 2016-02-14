385d8049f4363ca983e475399442b1b4b8cd5ab7
rrrgh <you@example.com>
2016-02-14 21:48:27 +0900

    find *.part files and download separately by extracting ID

--- dn_utuv.sh
+++ dn_utuv.sh
@@ -1,5 +1,13 @@
 #!/bin/zsh
-find /mnt/0 -type f -i '*dwn*' -execdir youtube-dl -a {} +
-#find /mnt/0 -type f -i '*dwn*' -print
-wait
-sudo shutdown -P -f +5
+
+#1. find *.part files and download separately by extracting ID
+#find /mnt/0 -type f -iname '*.part' -execdir youtube-dl --youtube-skip-dash-manifest --prefer-ffmpeg -R 19 -a {} +
+# extract the substring of 11 characters between last(or the one next to last) hyphen(-) and the first period(.) after that
+
+#2. URL list file
+find /mnt/0 -type f -iname 'dwn' -execdir youtube-dl --youtube-skip-dash-manifest -a {} +
+
+#wait
+
+sudo shutdown -P -f +3
+#sudo shutdown -P -f now
