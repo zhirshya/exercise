26d0a740dd1b985b8f4179c3c33fb2905804d3f0
rrrgh <you@example.com>
2016-02-15 23:24:45 +0900

    youtube-dl

--- dn_utuv.sh
+++ dn_utuv.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 #1. find *.part files and download separately by extracting ID
-#find /mnt/0 -type f -iname '*.part' -execdir youtube-dl --youtube-skip-dash-manifest --prefer-ffmpeg -R 19 -a {} +
+#find /mnt/0 -type f -iname '*.part' -execdir youtube-dl --youtube-skip-dash-manifest -a {} +
 # extract the substring of 11 characters between last(or the one next to last) hyphen(-) and the first period(.) after that
 
 #2. URL list file
