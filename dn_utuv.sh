7d8f577c69114927feaea6978212773836575264
rrrgh <you@example.com>
2016-02-20 11:50:40 +0900

    --prefer-ffmpeg -R 19

--- dn_utuv.sh
+++ dn_utuv.sh
@@ -5,7 +5,7 @@
 # extract the substring of 11 characters between last(or the one next to last) hyphen(-) and the first period(.) after that
 
 #2. URL list file
-find /mnt/0 -type f -iname 'dwn' -execdir youtube-dl --youtube-skip-dash-manifest -a {} +
+find /mnt/0 -type f -iname 'dwn' -execdir youtube-dl --youtube-skip-dash-manifest --prefer-ffmpeg -R 19 -a {} +
 
 #wait
 
