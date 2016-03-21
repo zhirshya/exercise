541f50ddaadcc3f12807f36ad886c31c10b8d800
rrrgh <you@example.com>
2016-03-21 19:14:52 +0900

    + separator

--- pull_gitrepo.sh
+++ pull_gitrepo.sh
@@ -1,102 +1,140 @@
 cd /mnt/0/linux
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/linux-insides
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/FunctionalPlus		#helps you write concise and readable C++ code
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/hacker-scripts
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/CppCoreGuidelines
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/g3log
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/g3sinks
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/constexpr
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/benchmark		#microbenchmark support library, google
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/You-Dont-Know-JS
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/Eloquent-JavaScript
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/v86
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/floating-point-gui.de
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/masteringnode
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/node
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
+cd /mnt/0/nodejs.org
+pwd
+git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/QueueNado
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/todomvc
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/react
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/react-native
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/bootlint
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/ModernCpp
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/java8-tutorial
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/javaee7-samples
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/hapi			#Server Framework for Node.js http://hapijs.com
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/MVARobotics
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/mean			#MEAN (Mongo, Express, Angular, Node) - A Simple, Scalable and Easy starting point for full stack javascript web development - utilizing many of the best practices we've found on the way http://mean.io
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/playframework
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/nan
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/macchina.io
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/tungstenjs
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/Raspberry_Pi_2_and_OpenCV_3_Tutorial_Part_1
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/"tutorials(REST With Spring Classes)"
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/india_ejb_
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
 cd /mnt/0/CppCon2015
 pwd
 git pull
+echo ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓ ∓
