9397ffe6f7d23b71b7305bdd30ae947b286a13e6
rrrgh <you@example.com>
2016-03-21 12:39:23 +0900

    Unicode Character 'PLUS-MINUS SIGN' (U+00B1)

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -5,5 +5,6 @@ int main()
 {
 	int x = 35;
 	printf("%d,%d,%d\n", x == 35, x == 50, x > 40);
-
+	std::cout << "Unicode Character 'PLUS-MINUS SIGN' (U+00B1):" << "\u00B1" << std::endl;
+	std::cout << "Unicode Character 'MINUS-OR-PLUS SIGN' (U+2213):" << "\u2213" << std::endl;
 }
