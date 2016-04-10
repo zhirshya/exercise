11c7b8e9474bf018a3f2dcee27260608407cbe22
rrrgh <you@example.com>
2016-04-10 20:19:54 +0900

    Public IP Address From Command Line

--- pbip.sh
+++ pbip.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-dig +short myip.opendns.com @resolver1.opendns.com
+#dig +short myip.opendns.com @resolver1.opendns.com
+#dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | awk -F'"' '{print $2}'
+#host myip.opendns.com resolver1.opendns.com
+
+ip_opendns="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+echo "WAN/Public IP address via opendns: ${ip_opendns}"
+
+ip_ggl="$(dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | awk -F'"' '{print $2}')"
+echo "WAN/Public IP address via ggl: ${ip_ggl}"
