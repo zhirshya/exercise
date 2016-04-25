f51e4d0bde9ab879628aa55130057e3b92d99556
rrrgh <you@example.com>
2016-04-25 22:32:38 +0900

    Shouldn't Use std::endl

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <stdio.h>
 
+namespace cds {
+    char const nl = '\n';
+}
+
 int main()
 {
+	using namespace std;
 	int x = 35;
 	printf("%d,%d,%d\n", x == 35, x == 50, x > 40);
-	std::cout << "Unicode Character 'PLUS-MINUS SIGN' (U+00B1):" << "\u00B1" << std::endl;
-	std::cout << "Unicode Character 'MINUS-OR-PLUS SIGN' (U+2213):" << "\u2213" << std::endl;
+	cerr << "Unicode Character 'PLUS-MINUS SIGN' (U+00B1):" << "\u00B1" << cds::nl;
+	cerr << "Unicode Character 'MINUS-OR-PLUS SIGN' (U+2213):" << "\u2213" << cds::nl;
 }
