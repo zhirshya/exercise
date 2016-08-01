b0ad0024626e1a1a8ff78adbb60b5661a0efa250
rrrgh <r@moskvo.sb>
2016-08-02 07:26:58 +0900

    xrc

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -5,6 +5,11 @@ namespace cds {
     char const nl = '\n';
 }
 
+enum minus {
+	int minval=-5,
+	maxval
+}
+
 int main()
 {
 	using namespace std;
@@ -12,4 +17,5 @@ int main()
 	printf("%d,%d,%d\n", x == 35, x == 50, x > 40);
 	cerr << "Unicode Character 'PLUS-MINUS SIGN' (U+00B1):" << "\u00B1" << cds::nl;
 	cerr << "Unicode Character 'MINUS-OR-PLUS SIGN' (U+2213):" << "\u2213" << cds::nl;
+	cout << "minus.maxval(uninit enum):" << minus.maxval << "\n";
 }
