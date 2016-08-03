7222e2a8470df2cb1ec0763d146bd1332e88e1e6
rrrgh <r@moskvo.sb>
2016-08-03 22:08:09 +0900

    penultimate dash loop

--- utv.sh
+++ utv.sh
@@ -2,9 +2,8 @@
 
 #1. find *.part files and download separately by extracting ID
 #find /media/r/0 -type f -iname '*.part' -execdir youtube-dl --youtube-skip-dash-manifest -a {} +
-# extract the substring of 11 characters between last(or the one next to last) hyphen(-) and the first period(.) after that
+# extract the substring of 11 characters between last/penultimate hyphen/dash(-) and the first period(.) after that
 
-#2. URL list file
 xt_code=-1
 while [ $xt_code -ne 0 ]; do
   find /media/r/0 -type f -iname 'dwn' -execdir youtube-dl --youtube-skip-dash-manifest --prefer-ffmpeg -R 19 -a {} +
@@ -21,7 +20,7 @@ if [[ $# -eq 1 ]] && [[ $1 =~ "[0-9]+" ]]; then
 #	fi 
 else
 #	timeout_arg="+3"
-	echo "Exiting script."
+	echo "exit script."
 	exit 0
 fi
 
