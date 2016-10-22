a742d6cfb8913172c0e1ee5abb2a37730667dece
rrrgh <rrrgh@monru.taiga.baigal.sb>
2016-10-22 14:37:16 +0900

    correction

--- utv.sh
+++ utv.sh
@@ -3,7 +3,10 @@
 #1. find *.part files and download separately by extracting ID
 #find /mnt/0 -type f -iname '*.part' -execdir youtube-dl --youtube-skip-dash-manifest -a {} +
 # extract the substring of 11 characters between last/penultimate hyphen/dash(-) and the first period(.) after that
+
+typeset -F SECONDS
 echo «$(\date)»
+
 xt_code=-1
 while [[ $xt_code -ne 0 ]];do
   find /mnt/0 -type f -iname 'dwn' -execdir youtube-dl --youtube-skip-dash-manifest --prefer-ffmpeg -R 21 -a {} +
@@ -12,12 +15,12 @@ while [[ $xt_code -ne 0 ]];do
 done
 
 echo «$(\date)»
-echo 'time':time
-echo 'times':times
+echo '$(time)':$(time)
+echo '$(times)':$(times)
 #or add a trap on EXIT, that way, times will be called whenever the shell exits and the exit status will be preserved.
 #trap times EXIT
 #bash, ksh and zsh have a $SECONDS special variable that counts the number of seconds since the shell was started. In both zsh and ksh93, that variable can also be made floating point (with typeset -F SECONDS) to get more precision. This is only wall clock time, not CPU time.
-typeset -F SECONDS
+#typeset -F SECONDS
 echo '$SECONDS':$SECONDS
 #http://unix.stackexchange.com/questions/52313/how-to-get-execution-time-of-a-script-effectively
 
