21f0ae61740d179afe9ff8467740967ead5ad874
rrrgh <rrrgh@monru.taiga.baigal.sb>
2016-10-22 10:53:06 +0900

    timing « »

--- utv.sh
+++ utv.sh
@@ -3,13 +3,15 @@
 #1. find *.part files and download separately by extracting ID
 #find /mnt/0 -type f -iname '*.part' -execdir youtube-dl --youtube-skip-dash-manifest -a {} +
 # extract the substring of 11 characters between last/penultimate hyphen/dash(-) and the first period(.) after that
-
+echo «\date»
 xt_code=-1
 while [[ $xt_code -ne 0 ]];do
   find /mnt/0 -type f -iname 'dwn' -execdir youtube-dl --youtube-skip-dash-manifest --prefer-ffmpeg -R 19 -a {} +
   xt_code=$?
   echo "exit code(find...-execdir youtube-dl...{} +):$xt_code"
 done
+
+echo «\date»
 
 #timeout_arg=now
 if [[ $# -eq 1 ]] && [[ "$1" =~ [0-9]+ ]];then
