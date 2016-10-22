c41923ce335012401a287bab15ef24fa987af1a9
rrrgh <rrrgh@monru.taiga.baigal.sb>
2016-10-22 12:38:52 +0900

    time times trap times exit

--- utv.sh
+++ utv.sh
@@ -6,22 +6,29 @@
 echo «$(\date)»
 xt_code=-1
 while [[ $xt_code -ne 0 ]];do
-  find /mnt/0 -type f -iname 'dwn' -execdir youtube-dl --youtube-skip-dash-manifest --prefer-ffmpeg -R 19 -a {} +
+  find /mnt/0 -type f -iname 'dwn' -execdir youtube-dl --youtube-skip-dash-manifest --prefer-ffmpeg -R 21 -a {} +
   xt_code=$?
   echo "exit code(find...-execdir youtube-dl...{} +):$xt_code"
 done
 
 echo «$(\date)»
+echo 'time':time
+echo 'times':times
+#or add a trap on EXIT, that way, times will be called whenever the shell exits and the exit status will be preserved.
+#trap times EXIT
+#bash, ksh and zsh have a $SECONDS special variable that counts the number of seconds since the shell was started. In both zsh and ksh93, that variable can also be made floating point (with typeset -F SECONDS) to get more precision. This is only wall clock time, not CPU time.
+typeset -F SECONDS
+echo '$SECONDS':$SECONDS
+#http://unix.stackexchange.com/questions/52313/how-to-get-execution-time-of-a-script-effectively
 
 #timeout_arg=now
-if [[ $# -eq 1 ]] && [[ "$1" =~ [0-9]+ ]];then
+if (($# == 1)) && [[ "$1" =~ [0-9]+ ]];then
 	timeout_arg=+$1
 #	elif [[ $1 -eq 's' ]]; then #stand-by
-#		exit 0
+#		exit $xt_code
 #	fi 
 else
-#	timeout_arg="+3"
-	exit 0
+	exit $xt_code
 fi
 
 sudo shutdown -P -f $timeout_arg
