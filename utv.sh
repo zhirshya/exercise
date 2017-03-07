643c71522dba58b90829867c80e5ce208cd76e3f
rrrgh <rrrgh@monru.taiga.baigal.sb>
2017-03-07 23:18:36 +0900

    +getopts

--- utv.sh
+++ utv.sh
@@ -1,12 +1,31 @@
 #!/bin/zsh
 
-#lock screen and power off display
-dbus-send --type=method_call --dest=org.gnome.ScreenSaver /org/gnome/ScreenSaver org.gnome.ScreenSaver.Lock
-xset dpms force off
-
-#1. find *.part files and download separately by extracting ID
+#todo find *.part files and download separately by extracting ID
 #find /mnt/0 -type f -iname '*.part' -execdir youtube-dl --youtube-skip-dash-manifest -a {} +
-# extract the substring of 11 characters between last/penultimate hyphen/dash(-) and the first period(.) after that
+#extract the substring of 11 characters between last/penultimate hyphen/dash(-) and the first period(.) after that
+
+lockpoweroff_screen=on
+shutdown_timeout=0
+
+while getopts lp: opt;do
+	case "$opt" in
+	l) lockpoweroff_screen="$OPTARG";; #0(zero) or "off"
+	p) shutdown_timeout="$OPTARG";;
+	\?) # unknown flag
+		echo >&2 \
+		"usage: $0 [-l [off|0]] [-p positive_integer]"
+		exit 1;;
+	esac
+done
+shift $((OPTIND-1))
+
+if !([[ $lockpoweroff_screen =~ off ]] || [[ $lockpoweroff_screen =~ 0 ]] || (($lockpoweroff_screen == 0)));then
+#if (($# == 1)) && [[ "$1" =~ lock ]];then
+	#lock screen and power off display
+	#busctl --user set-property org.gnome.Mutter.DisplayConfig /org/gnome/Mutter/DisplayConfig org.gnome.Mutter.DisplayConfig PowerSaveMode i 3
+	dbus-send --type=method_call --dest=org.gnome.ScreenSaver /org/gnome/ScreenSaver org.gnome.ScreenSaver.Lock
+	xset dpms force off
+fi
 
 typeset -F SECONDS
 echo "(start)«$(\date)»"
@@ -28,14 +47,12 @@ echo '$(times)':$(times)
 echo '$SECONDS':$SECONDS
 #http://unix.stackexchange.com/questions/52313/how-to-get-execution-time-of-a-script-effectively
 
-#timeout_arg=now
-if (($# == 1)) && [[ "$1" =~ [0-9]+ ]];then
-	timeout_arg=+$1
-#	elif [[ $1 -eq 's' ]]; then #stand-by
-#		exit $xt_code
-#	fi 
+#if (($# == 1)) && [[ "$1" =~ [0-9]+ ]];then
+#	shutdown_timeout=+$1
+##	elif [[ $1 -eq 's' ]]; then #stand-by
+##		exit $xt_code
+if [[ $shutdown_timeout =~ now ]] || [[ $shutdown_timeout =~ [0-9]+ ]];then
+	sudo shutdown -P -f $shutdown_timeout
 else
 	exit $xt_code
 fi
-
-sudo shutdown -P -f $timeout_arg
