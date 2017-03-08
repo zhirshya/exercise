32659d6bd5d5f39a89e590194737acb67e5275e0
rrrgh <rrrgh@monru.taiga.baigal.sb>
2017-03-08 22:42:29 +0900

    bug fix

--- utv.sh
+++ utv.sh
@@ -4,10 +4,10 @@
 #find /mnt/0 -type f -iname '*.part' -execdir youtube-dl --youtube-skip-dash-manifest -a {} +
 #extract the substring of 11 characters between last/penultimate hyphen/dash(-) and the first period(.) after that
 
-lockpoweroff_screen=on
+lockpoweroff_screen="on"
 shutdown_timeout=0
 
-while getopts lp: opt;do
+while getopts l:p: opt;do
 	case "$opt" in
 	l) lockpoweroff_screen="$OPTARG";; #0(zero) or "off"
 	p) shutdown_timeout="$OPTARG";;
@@ -19,10 +19,8 @@ while getopts lp: opt;do
 done
 shift $((OPTIND-1))
 
-if !([[ $lockpoweroff_screen =~ off ]] || [[ $lockpoweroff_screen =~ 0 ]] || (($lockpoweroff_screen == 0)));then
-#if (($# == 1)) && [[ "$1" =~ lock ]];then
-	#lock screen and power off display
-	#busctl --user set-property org.gnome.Mutter.DisplayConfig /org/gnome/Mutter/DisplayConfig org.gnome.Mutter.DisplayConfig PowerSaveMode i 3
+if [[ $lockpoweroff_screen != off ]] && [[ $lockpoweroff_screen != 0 ]];then
+#if ! ( [[ $lockpoweroff_screen == off ]] || [[ $lockpoweroff_screen == 0 ]] || (($lockpoweroff_screen == 0)) );then #NOT work
 	dbus-send --type=method_call --dest=org.gnome.ScreenSaver /org/gnome/ScreenSaver org.gnome.ScreenSaver.Lock
 	xset dpms force off
 fi
@@ -47,11 +45,7 @@ echo '$(times)':$(times)
 echo '$SECONDS':$SECONDS
 #http://unix.stackexchange.com/questions/52313/how-to-get-execution-time-of-a-script-effectively
 
-#if (($# == 1)) && [[ "$1" =~ [0-9]+ ]];then
-#	shutdown_timeout=+$1
-##	elif [[ $1 -eq 's' ]]; then #stand-by
-##		exit $xt_code
-if [[ $shutdown_timeout =~ now ]] || [[ $shutdown_timeout =~ [0-9]+ ]];then
+if [[ $shutdown_timeout == now ]] || [[ $shutdown_timeout =~ [0-9]+ ]];then
 	sudo shutdown -P -f $shutdown_timeout
 else
 	exit $xt_code
