2ba2b0af924aca0835f126bfe4105253c09feeca
rrrgh <rrrgh@monru.taiga.baigal.sb>
2017-04-02 16:18:26 +0900

    + -r 75k

--- utv.sh
+++ utv.sh
@@ -1,20 +1,21 @@
 #!/bin/zsh
 
 #todo find *.part files and download separately by extracting ID
-#find /mnt/0 -type f -iname '*.part' -execdir youtube-dl --youtube-skip-dash-manifest -a {} +
+#find /mnt/0 ~ -type f -iname '*.part' -execdir youtube-dl --youtube-skip-dash-manifest -a {} +
 #extract the substring of 11 characters between last/penultimate hyphen/dash(-) and the first period(.) after that
 
 lockpoweroff_screen="on"
 shutdown_timeout="NOW"
 #shutdown_timeout=0
 
-while getopts l:s: opt;do
+while getopts "l:s:b:" opt; do
 	case "$opt" in
 	l) lockpoweroff_screen="$OPTARG";; #0(zero) or "off"
 	s) shutdown_timeout="$OPTARG";;
+	b) bandwidth_rate="$OPTARG";;
 	\?) # unknown flag
 		echo >&2 \
-		"usage: $0 [-l [off|0]] [-s positive_integer]"
+		"usage: $0 [-l [off|0]] [-s positive_integer] [-b positive_integer[kKmM]]"
 		exit 1;;
 	*) echo "default case of getopts, nothing to do";;
 	esac
@@ -32,9 +33,31 @@ echo "(start)«$(\date)»"
 
 xt_code=-1
 while [[ $xt_code -ne 0 ]];do
-  find /mnt/0 -type f -iname 'dwn' -execdir youtube-dl --youtube-skip-dash-manifest --prefer-ffmpeg -a {} +
-  xt_code=$?
-  echo "exit code(find...-execdir youtube-dl...{} +):$xt_code"
+	#https://www.gnu.org/software/bash/manual/bash.html#Bash-Conditional-Expressions
+	#-v varname: True if the shell variable varname is set (has been assigned a value).
+	#-z string: True if the length of string is zero.
+	#-n string:
+	#   string: True if the length of string is non-zero.
+
+	#http://zsh.sourceforge.net/Doc/Release/Conditional-Expressions.html
+	#if [[ ! -v bandwidth_rate ]] || [[ -z $bandwidth_rate ]];then  #unknown condition: -v #zsh 5.2 (x86_64-redhat-linux-gnu)
+	#if [[ ! -v $bandwidth_rate ]] || [[ -z $bandwidth_rate ]];then  #unknown condition: -v #zsh 5.2 (x86_64-redhat-linux-gnu)
+	if [[ -z $bandwidth_rate ]];then
+		echo '#hit [[ -z $bandwidth_rate ]]'
+		find /mnt/0 ~ -type f -iname 'dwn' -execdir youtube-dl --youtube-skip-dash-manifest --prefer-ffmpeg -a {} +
+	else
+		if [[ ! $bandwidth_rate =~ ^[0-9]+[kKmM]$ ]];then
+			if [[ $bandwidth_rate =~ ^[0-9]+$ ]];then
+				bandwidth_rate+='k'
+			else
+				bandwidth_rate='75k'
+			fi
+		fi
+		echo "#bandwidth_rate":$bandwidth_rate
+		find /mnt/0 ~ -type f -iname 'dwn' -execdir youtube-dl --youtube-skip-dash-manifest --prefer-ffmpeg -r ${bandwidth_rate} -a {} +
+	fi
+	xt_code=$?
+	echo "exit code(find...-execdir youtube-dl...{} +):$xt_code"
 done
 
 echo "(end)«$(\date)»"
