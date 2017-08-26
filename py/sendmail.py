c092600cb38d68aebceef9f894c5780c1d344761
rrrgh <rrrgh@monru.taiga.baigal.sb>
2017-08-26 23:03:43 +0900

    send mail with attachments

---
+++ py/sendmail.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python -tt
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import string
+import smtplib
+import configparser
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email import Encoders
+from email.header import Header
+from email.Utils import formatdate
+from email import Charset
+from email.generator import Generator
+from cStringIO import StringIO
+ 
+def send_email(mailInfoFile):
+    if os.path.exists(mailInfoFile):
+        config = configparser.ConfigParser()
+        config.read(mailInfoFile)
+        from_emails = config['DEFAULT']['from']
+        to_emails = config['DEFAULT']['to']
+        cc_emails = config['DEFAULT']['cc']
+        bcc_emails = config['DEFAULT']['bcc']
+        subject = config['DEFAULT']['subject']
+        body = config['DEFAULT']['body']
+        attachments = config['DEFAULT']['attachment']
+        passwd = config['DEFAULT']['passwd']
+        smtpServer = config['DEFAULT']['smtpServer']
+        port = config['DEFAULT']['port']
+
+        #debug
+        print 'from_emails:',from_emails
+        print 'to_emails:',to_emails
+        print 'cc_emails:',cc_emails
+        print 'bcc_emails:',bcc_emails
+        print 'subject:',subject
+        print 'body:',body
+        print 'attachments:',attachments
+        print 'passwd:',passwd
+        print 'smtpServer:',smtpServer
+        print 'port:',port
+
+        #construct mail
+        msg = MIMEMultipart()
+        msg['From'] = from_emails
+        #msg['To'] = ', '.join(to_emails)
+        msg['To'] = to_emails
+        #msg['cc'] = ', '.join(cc_emails)
+        msg['cc'] = cc_emails
+        #msg['bcc'] = ', '.join(bcc_emails)
+        msg['bcc'] = bcc_emails
+
+        emails = []
+        emails.append(to_emails)
+        emails.append(cc_emails)
+        emails.append(bcc_emails)
+
+        msg['Date'] = formatdate(localtime=True)
+
+        #todo1:
+        #Default encoding mode set to Quoted Printable. Acts globally!
+        Charset.add_charset('utf-8', Charset.QP, Charset.QP, 'utf-8')
+        
+        msg['Subject'] = '{}'.format(Header(subject,'utf-8'))
+        if body:
+            msg.attach(MIMEText(body,'plain','utf-8'))
+
+        try:
+            attachmentList = attachments.split(',')
+            for file in attachmentList:
+                file = file.strip()
+                with open(file, 'rb') as fos:
+                    data = fos.read()
+                msgAttachment = MIMEBase('application', 'octet-stream')
+                msgAttachment.set_payload(data)
+                Encoders.encode_base64(msgAttachment)
+                msgAttachment.add_header('Content-Disposition', 'attachment; filename="{}"'.format(os.path.basename(file)))
+                msg.attach(msgAttachment)
+        except IOError, e:
+        #todo2:
+        #better detailed exception info
+            msg = 'Error opening attachment file "{}"'.format(file)
+            print 'except e:',e
+            print 'os.path.basename(file)',os.path.basename(file)
+            print msg
+            sys.exit(1)
+
+        #type(port): <type 'unicode'>
+        print 'type(port):',type(port)
+        smtpObj = smtplib.SMTP(smtpServer, port.encode('ascii','ignore'))
+        smtpObj.ehlo()
+        smtpObj.starttls()
+        smtpObj.login(from_emails, passwd)
+        smtpObj.sendmail(from_emails, emails, msg.as_string())
+        smtpObj.quit()
+    else:
+        print 'Config file not found! Exiting!'
+        sys.exit(1)
+
+if __name__ == '__main__':
+    print(sys.argv)
+    send_email(sys.argv[1])
