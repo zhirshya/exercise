64456276ec833bdb851aa6b91efc08d1a1ca2397
rrrgh <rrrgh@monru.taiga.baigal.sb>
2017-08-27 09:06:24 +0900

    refactor

--- py/linux_release.check.py
+++ py/linux_release.check.py
@@ -4,18 +4,46 @@
 from lxml import html
 import requests
 
+osList = []
+osinfo = {'arch':'', 'ver':'', 'url':'', 'sha256sum':''}
+
+#kali
 page = requests.get('https://www.kali.org/downloads/')
 tree = html.fromstring(page.content)
 
 #todo
 #better parse the whole table and find x86-64 arch's version info!
 
-kali_architecture = tree.xpath('/html/body/div[1]/div[2]/div/main/section/div/div/div/div/div/div[5]/div/div/div/div/div/table/tbody/tr[2]/td[1]/text()')
-kali_release_version = tree.xpath('/html/body/div[1]/div[2]/div/main/section/div/div/div/div/div/div[5]/div/div/div/div/div/table/tbody/tr[2]/td[4]/strong/text()')
-sha256sum = tree.xpath('/html/body/div[1]/div[2]/div/main/section/div/div/div/div/div/div[5]/div/div/div/div/div/table/tbody/tr[2]/td[5]/text()')
 #selector
 #body > div.l-canvas.sidebar_right.type_wide.titlebar_default > div.l-main > div > main > section > div > div > div > div > div > div:nth-child(5) > div > div > div > div > div > table > tbody > tr:nth-child(2) > td:nth-child(4) > strong
 
-print 'kali_architecture',kali_architecture
-print 'kali_release_version:',kali_release_version
-print 'sha256sum:',sha256sum
+raw_html_content = tree.xpath('/html/body/div[1]/div[2]/div/main/section/div/div/div/div/div/div[5]/div/div/div/div/div/table/tbody/tr[2]/td[1]/text()')
+for field in raw_html_content:
+    field = field.strip().replace('\n','').replace('\r','')
+    if field:
+        osinfo['arch'] = field
+
+raw_html_content = tree.xpath('/html/body/div[1]/div[2]/div/main/section/div/div/div/div/div/div[5]/div/div/div/div/div/table/tbody/tr[2]/td[4]/strong/text()')
+for field in raw_html_content:
+    field = field.strip().replace('\n','').replace('\r','')
+    if field:
+        osinfo['ver'] = field
+
+raw_html_content = tree.xpath('/html/body/div[1]/div[2]/div/main/section/div/div/div/div/div/div[5]/div/div/div/div/div/table/tbody/tr[2]/td[5]/text()')
+for field in raw_html_content:
+    field = field.strip().replace('\n','').replace('\r','')
+    if field:
+        osinfo['sha256sum'] = field
+
+osList.append(osinfo.copy())
+
+#linux mint
+
+#knoppix
+
+#print 'kali_architecture',kali_architecture
+#print 'kali_release_version:',kali_release_version
+#print 'sha256sum:',sha256sum
+for osItem in osList:
+    print osItem
+
