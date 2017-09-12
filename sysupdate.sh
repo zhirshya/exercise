0b9b3507d4786863dff80a63ffd9e31b59dd6da2
rrrgh <rrrgh@monru.taiga.baigal.sb>
2017-09-12 22:39:35 +0900

    systemd script for update system

--- sysupdate.sh
+++ sysupdate.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+exec  &>> /mnt/0/workspace/sysupdate.log
+
 echo «$(\date +"%Y%m%d %H%M%S.%N%::z%Z %A")»
 sudo dnf upgrade -y
 echo '$?':$?
 sudo youtube-dl -U
 echo '$?':$?
+blutu0
+
 echo «$(\date +"%Y%m%d %H%M%S.%N%::z%Z %A")»
