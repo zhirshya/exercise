2fc0304766e6276b841e25d4076bc1c407909085
rrrgh <rrrgh@monru.taiga.baigal.sb>
2017-12-05 06:56:58 +0900

    c++ cli sum

---
+++ cpp/argv_sum.cpp
@@ -0,0 +1,4 @@
+#c++17 CLI budget calculator
+#regex replace [xX] -> *; put in HashMap (like Linux uniq); map: e.g. '96 [xX*] 3'|'96*3' -> (big decimal)96*3; reduce: + (sum all numeric elements of HashMap?); *1.08|1.1|1.12|1.15
+#Rang:Minimal, Header only Modern c++ library for colors in your terminal
+#python to c++17 converter
