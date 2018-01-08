78814731c554be2ef9dbec3bc9244d2638031420
rrrgh <rrrgh@monru.taiga.baigal.sb>
2018-01-08 15:39:07 +0900

    Fast I/O for Competitive Programming

---
+++ cpp/c++bareminimum.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+//https://www.geeksforgeeks.org/fast-io-for-competitive-programming/
+
+using namespace std;
+
+int main(int argc, char** argv){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
