c0e58394b2078e38759bfe6265981023302e9763
rrrgh <rrrgh@monru.taiga.baigal.sb>
2018-01-08 15:09:13 +0900

    mt19937

---
+++ cpp/randomIntgr.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+//https://www.geeksforgeeks.org/random-header-c-set-1generators/
+//https://www.geeksforgeeks.org/c-program-generate-random-number/
+
+// Function with return random number from 1 to given limit
+int randomNoGenerator(int limit)
+{
+    // uniformly-distributed integer random number generator that produces non-deterministic random numbers.
+    random_device rd;
+ 
+    // A Mersenne Twister pseudo-random generator of 32-bit numbers with a state size of 19937 bits.
+    mt19937 gen(rd());
+ 
+    // Uniform distribution
+    uniform_int_distribution<> dis(1, limit);
+    return dis(gen);
+}
+ 
+// Driver Code
+int main()
+{
+    int limit = 19937;
+    cout << randomNoGenerator(limit) << '\n';
+    return 0;
+}
