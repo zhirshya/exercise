1896d9761c6232ceda131875a25ad1fd4c569ef9
Zhirshya <prsvrrgtd@outlook.com>
2018-08-07 14:47:23 +0800

    https://www.slideshare.net/masatakakondo16/how-to-build-libreoffice-on-your-desktop-81057881

---
+++ lo-ubuntu-prerequisites.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sudo apt update && sudo apt dist-upgrade
+#apt search libkrb*
+sudo apt install g++ autotools-dev automake libcups2-dev libfontconfig1-dev gperf doxygen comerr-dev libkrb5-dev libxslt1-dev xsltproc libxml2-utils python3-dev python-dev libx11-dev libxt-dev libxext-dev libxrender-dev libxrandr-dev libgtk-3-dev libdbus-glib-1-dev libgtk2.0-dev libglew-dev ant junit4 nasm libgcrypt11-dev
